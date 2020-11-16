d8a1339ebd54f6fb118227bdbe98a078851757e0
TiberiuMTC <tiberiu.mihai.22.97@gmail.com>
2020-11-16 12:56:43 +0200

    Modificare clasa Biblioteca
    Adaugare metoda remoceBook
    
    Signed-off-by: TiberiuMTC <tiberiu.mihai.22.97@gmail.com>

--- src/main/java/Biblioteca.java
+++ src/main/java/Biblioteca.java
@@ -1,20 +1,24 @@
 import java.util.ArrayList;
 
 public class Biblioteca {
-    private ArrayList<Carte> stoc=new ArrayList<Carte>();
+    private ArrayList<Carte> ListaCarti =new ArrayList<Carte>();
 
     public Biblioteca(Carte in_book){
-        this.stoc.add(in_book);
+        this.ListaCarti.add(in_book);
     }
     public void add(Carte in_obj){
-        this.stoc.add(in_obj);
+        this.ListaCarti.add(in_obj);
     }
 
     @Override public String toString(){
         String display="";
-        for (int i=0;i<stoc.size();i++){
-            display+=this.stoc.get(i).toString();
+        for (int i = 0; i< ListaCarti.size(); i++){
+            display+=this.ListaCarti.get(i).toString();
         }
         return display;
+    }
+
+    public void remove_book(Carte c){
+        this.ListaCarti.remove(c);
     }
 }
